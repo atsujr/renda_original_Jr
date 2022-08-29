7783bc000d7096b1e18d0bbc739842dd695ed94d
Atsuhiro Muroyama <atuhiro.duda@gmail.com>
2022-08-29 14:05:53 +0900

    Initial Commit

---
+++ renda_original/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  renda_original
+//
+//  Created by Atsuhiro Muroyama on 2022/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
